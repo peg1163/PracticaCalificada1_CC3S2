9ece650a4378056d8f0921d7dbd5ab9b979d3d6e
peg1163 <jaime.acuna.n@uni.pe>
2023-09-29 13:17:26 -0500

    rerun

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'puma'
 gem 'sinatra'
 group :development do
     gem 'rerun'
--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-require 'sinatra' 
-    class MyApp < Sinatra::Base 
-        get '/' do 
-        "<!DOCTYPE html><html><head></head><body><h1>Godbye wold!</h1></body></html>" 
+require 'sinatra'
+    class MyApp < Sinatra::Base
+        get '/' do
+        "<!DOCTYPE html><html><head></head><body><h1>Hello wold!</h1></body></html>"
     end
 end
